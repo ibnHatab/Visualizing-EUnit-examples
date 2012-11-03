7fc381cd4a5affbba3d629547b94cf111b723273
VKI <you@example.com>
2012-11-03 10:40:03 +0200

    prepare merge

--- src/tr69_filter.erl
+++ src/tr69_filter.erl
@@ -4,7 +4,9 @@
          clear/1]).
 
 start() ->
-    start([]).
+    start([
+	   {actors, [client, timer, us, peer, httpc, httpd] }
+	  ]).
 
 start(ExtraOptions) ->
     Options =
@@ -12,7 +14,6 @@ start(ExtraOptions) ->
          {scale, 2},
          {max_actors, 10},
          {detail_level, 90},
-         {actors, [client, timer, us, peer]},
          {trace_pattern, {tr69, max}},
          {trace_global, true},
          {title, "TR-069 tracer"} | ExtraOptions],
