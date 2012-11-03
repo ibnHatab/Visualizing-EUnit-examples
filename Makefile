75e277530a0b538a35800c11c2690545eac67a3d
VKI <you@example.com>
2012-11-03 13:28:14 +0200

    lager app test

--- Makefile
+++ Makefile
@@ -26,14 +26,16 @@ docs:
 	@erl -noshell -run edoc_run application '$(APP)' '"."' '[]'
 
 utest:
-	$(REBAR) -v eunit skip_deps=true suite=tr_soap_types
+	$(REBAR) -v eunit skip_deps=true ## suite=tr_soap_types
+
+ctest: 
+	$(REBAR) -v compile ct skip_deps=true suites=tr69_trace case=app_loging_tc
 
 webstart: app
 	exec erl -pa $(PWD)/ebin -pa $(PWD)/deps/*/ebin 	\
-		-boot start_sasl 		\
-		-s lager -s tr69	 					\
+		-config priv/sys.config				\
+		-boot start_sasl -s lager -s tr69		\
 		|| echo Exit
-
 
 .PHONY: ctest
 
--- src/tr69.erl
+++ src/tr69.erl
@@ -1,9 +1,12 @@
 %%% File    : tr69.erl
-%%% Description : 
+%%% Description :
 %%%   Manual startup of the TR-069 application
 
 
 -module(tr69).
+
+-compile({parse_transform, lager_transform}).
+
 
 -author('author <author@example.com>').
 -export([start/0, start_link/0, stop/0]).
@@ -16,34 +19,39 @@ ensure_started(App) ->
         ok ->
             ok;
         {error, {already_started, App}} ->
-            ok
+            ok;
+        {error, Reason} ->
+	    erlang:error({app_start_failed, App, Reason}),
+            {error, Reason}
     end.
 
 
 %% @spec start_link() -> {ok,Pid::pid()}
 %% @doc Starts the app for inclusion in a supervisor tree
 start_link() ->
-    ensure_started(crypto),
+    ok = ensure_started(sasl),
+    ok = ensure_started(lager),
+    ok = ensure_started(gproc),
     acs_core_sup:start_link().
 
 %% @spec start() -> ok
-%% @doc Start the acs_core server.
+%% @doc Start the tr69 for testing.
 start() ->
-    ensure_started(sasl),
-    ensure_started(gproc),
-    ensure_started(lager),
-    application:start(tr69).
+    ok = ensure_started(sasl),
+    ok = lager:start(),
+    ok = ensure_started(gproc),
+    ok = application:start(tr69).
 
 %% @spec stop() -> ok
 %% @doc Stop the acs_core server.
 stop() ->
     Res = application:stop(tr69),
-    application:stop(lager),
     application:stop(gproc),
+    application:stop(lager),
     application:stop(sasl),
     Res.
 
-%% ET 
+%% ET
 report_event(DetailLevel, FromTo, Label, Contents) ->
     %% N.B External call
     ?MODULE:report_event(DetailLevel, FromTo, FromTo, Label, Contents).
