875687d0dc59679cad39e60d75c92dd8a710c922
VKI <you@example.com>
2012-11-07 10:44:01 +0200

    sysutils

--- src/tr69_utils.erl
+++ src/tr69_utils.erl
@@ -1,9 +1,33 @@
-%%% File    : tr_utils.erl
+%%% File    : tr69_utils.erl
 %%% Description : Miscellaneous utilities for protocol processing.
 
 
 -module(tr69_utils).
 
--export([]).
+-export([is_arch/1]).
+
+%% Returns a forced-lowercase architecture for this node
+-spec get_arch () -> string().
+get_arch () -> string:to_lower(erlang:system_info(system_architecture)).
+
+%% Checks if this node is of a given architecture
+-spec is_arch (atom()) -> boolean().
+is_arch (linux) -> string:str(get_arch(),"linux") > 0;
+is_arch (darwin) -> string:str(get_arch(),"darwin") > 0;
+is_arch (sunos) -> string:str(get_arch(),"sunos") > 0;
+is_arch (osx) -> is_arch(darwin);
+is_arch (solaris) -> is_arch(sunos);
+is_arch (Arch) -> throw({unsupported_architecture,Arch}).
 
 
+
+
+%% ===================================================================
+%% EUnit tests
+%% ===================================================================
+-ifdef(TEST).
+
+is_arch_test() ->
+    ?assert(is_arch (linux)).
+
+-endif.
