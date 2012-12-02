e7a39b1b1747f127295bbb15331bb0bb8af88e1c
VKI <you@example.com>
2012-12-02 11:38:54 +0200

    two frequence test

--- test/frequency_tests.erl
+++ test/frequency_tests.erl
@@ -11,14 +11,15 @@
 -import(frequency,[start/1, stop/0, allocate/0, deallocate/1, skip/0, use_skip/0]).
 
 
+-define(FQ_POOL, [1,2]).
 
 %% %% Four positive tests.
 %% %% + start stop start stop
 start_stop_test_() ->
     {inorder,
-     [ ?_assertMatch(true,start([1])),
+     [ ?_assertMatch(true,start(?FQ_POOL)),
        ?_assertMatch(ok,stop()),
-       ?_assertMatch(true,start([1])),
+       ?_assertMatch(true,start(?FQ_POOL)),
        ?_assertMatch(ok,stop())]}.
 
 
@@ -31,16 +32,17 @@ stopFirst_test() ->
 %% - start start
 startTwice_test_() ->
   [{setup,
-    fun ()  -> start([1]) end,
+    fun ()  -> start(?FQ_POOL) end,
     fun (_) -> stop() end,
-      ?_assertError(badarg,start([1]))
+      ?_assertError(badarg,start(?FQ_POOL))
    }].
+
 
 %% A single allocate
 %% + start allocate deallocate allocate stop
 allocate_test_() ->
     {setup,
-     fun () -> start([1]) end,
+     fun () -> start(?FQ_POOL) end,
      fun(_) -> stop() end,
      fun () ->
 	     ?assertMatch({ok,1},allocate()),
@@ -50,11 +52,11 @@ allocate_test_() ->
 
 %% No more resources
 %% - start allocate allocate
-out_of_frequency_test_() ->
+out_of_frequency_test_one_fq() ->
     {setup,
      fun () -> ok end,
      fun(_) -> stop() end,
-     fun () ->  ?assertMatch(true,start([1])),
+     fun () ->  ?assertMatch(true,start(?FQ_POOL)),
 		?assertMatch({ok,1},allocate()),
 		?assertError({badmatch,{error,no_frequency}},allocate())
      end}.
@@ -69,7 +71,7 @@ allocate_without_start_test() ->
 %% - start deallocate
 start_deallocate_test_() ->
     {setup,
-     fun () -> start([1]) end,
+     fun () -> start(?FQ_POOL) end,
      fun(_) -> stop() end,
      fun () ->  
 	     ?assertError({badmatch,{error,not_allocated}}, deallocate(1))
@@ -79,19 +81,83 @@ start_deallocate_test_() ->
 % - start allocate start
 start_after_allocate_test_() ->
     {setup,
-     fun () -> start([1]) end,
+     fun () -> start(?FQ_POOL) end,
      fun(_) -> stop() end,
      fun () ->
 	     ?assertMatch({ok,1},allocate()),
-	     ?assertError(badarg, start([1]))
+	     ?assertError(badarg, start(?FQ_POOL))
      end
     }.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Two freq pool
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%% + start allocate allocate deallocate allocate deallocate deallocate stop
+allocate_deallocate_test_() ->
+    {setup,
+     fun () -> start(?FQ_POOL) end,
+     fun(_) -> stop() end,
+     fun () -> 
+	     ?assertMatch({ok,1},allocate()),
+	     ?assertMatch({ok,2},allocate()),
+	     ?assertMatch(ok,deallocate(1)),
+	     ?assertMatch({ok,1},allocate()),
+	     ?assertMatch(ok,deallocate(1)),
+	     ?assertMatch(ok,deallocate(1))
+     end}.
+
+%% - start allocate allocate allocate
+out_of_frequency_test_() ->
+    {setup,
+     fun () -> start(?FQ_POOL) end,
+     fun(_) -> stop() end,
+     fun () -> 
+		?assertMatch({ok,1},allocate()),
+		?assertMatch({ok,2},allocate()),
+		?assertError({badmatch,{error,no_frequency}},allocate())
+     end}.
+
+
+%% + start allocate allocate stop start
+stop_allocate_start_test_() ->
+    {setup,
+     fun () -> start(?FQ_POOL) end,
+     fun(_) -> stop() end,
+     fun () -> 
+	     ?assertMatch({ok,1},allocate()),
+	     ?assertMatch({ok,2},allocate()),
+	     ?assertMatch(ok,stop()),
+	     ?assertMatch(true,start(?FQ_POOL))
+     end}.
+
+
+%% - start allocate deallocate deallocate
+double_deallocate_test_() ->
+    {setup,
+     fun () -> start(?FQ_POOL) end,
+     fun(_) -> stop() end,
+     fun () -> 
+	     ?assertMatch({ok,1},allocate()),
+	     ?assertMatch(ok,deallocate(1)),
+	     ?assertError({badmatch,{error,not_allocated}}, deallocate(1))
+     end}.
+
+%% - start allocate allocate start
+start_after_allocate_twice_test_() ->
+    {setup,
+     fun () -> start(?FQ_POOL) end,
+     fun(_) -> stop() end,
+     fun () ->
+	     ?assertMatch({ok,1},allocate()),
+	     ?assertMatch({ok,2},allocate()),
+	     ?assertError(badarg, start(?FQ_POOL))
+     end
+    }.
 
 %% running_server_test_() ->
 %%     {foreach,
-%%      fun () -> start([1]) end,
+%%      fun () -> start(?FQ_POOL) end,
 %%      fun(_) -> stop() end,
 %%      [
 %%       fun() ->
@@ -106,7 +172,7 @@ start_after_allocate_test_() ->
 %%       fun() ->
 %% 	      ?assertMatch({ok,1},allocate()),
 %% 	      ?assertMatch(ok,stop()),
-%% 	      ?assertMatch(true,start([1]))
+%% 	      ?assertMatch(true,start(?FQ_POOL))
 %%       end  
 
 %%      ]
@@ -134,7 +200,7 @@ start_after_allocate_test_() ->
 %      fun () -> ok end,
 %      fun(_) -> stop() end,
 %      fun () ->
-% 	     ?assertMatch(true,start([1])),
+% 	     ?assertMatch(true,start(?FQ_POOL)),
 % 	     ?assertMatch({ok,1},allocate()),
 % 	     ?assertError({badmatch,{error,no_frequency}},allocate())
 %      end}.
@@ -153,14 +219,14 @@ start_after_allocate_test_() ->
 %      fun () -> ok end,
 %      fun(_) -> stop() end,
 %      fun () ->
-% 	     ?assertMatch(true,start([1])),
+% 	     ?assertMatch(true,start(?FQ_POOL)),
 % 	     ?assertMatch(ok,stop()),
 % 	     ?assertMatch(true,start([1, 2]))
 %      end}.
 
 
 % new_test() ->
-%   ?assertMatch(true,frequency_eqc:start([1])),
+%   ?assertMatch(true,frequency_eqc:start(?FQ_POOL)),
 %   ?assertMatch(ok,frequency_eqc:stop()),
 %   ?assertMatch(true,frequency_eqc:start([1, 2])).
 
@@ -171,7 +237,7 @@ start_after_allocate_test_() ->
 
 
  % new_test() ->
- %   ?assertMatch(true,start([1])),
+ %   ?assertMatch(true,start(?FQ_POOL)),
  %   ?assertMatch({ok,1},allocate()),
  %   ?assertError({badmatch,{error,no_frequency}},allocate()).
 
